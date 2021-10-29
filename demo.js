3d4e96af9de36c02106c21172999649b85079323
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2021-10-29 00:27:24 +0000

    Deploying to gh-pages from @ nobody5050/craft-engine@d5e2993d6107c019ac4d2326362b71b633ee46b5 🚀

--- demo.js
+++ demo.js
@@ -1,107 +1,170 @@
-/* global $, GameController, levels, Sounds */
+var previous = false;
 
-var defaults = {
-  assetPacks: {
-    beforeLoad: ['allAssetsMinusPlayer', 'playerAlex', 'playerAgent'],
-    afterLoad: [],
-  },
-  gridDimensions: [10, 10],
-  fluffPlane: ["","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""],
-  playerName: 'Alex',
-  playerStartPosition: [],
+var levelSelect;
+var speedSlider;
+var speedDisplay;
+var phaserGame;
+var codeMirrorEditor;
+var game;
+var playButton;
+var consolePre;
+var consoleErrors;
+var blockType;
+var codeMirror;
+var api;
+var gameController;
+
+var defaultScript = `/*
+ * Welcome to the craft-engine editor!
+ * 
+ * Some tips to get started:
+ *  + All the methods you need to solve the puzzles are in the ${"`api`"} object.
+ *  + You can use ${"`api.log`"}, ${"`api.error`"} and ${"`api.warning`"} to log info, errors, and warnings
+ *    in the console below.
+ *  + More tips coming soon with proper documentation :)
+ * 
+ */
+
+api.log("Hi!");
+
+// Spawns a Cow in the at (4,4) facing south
+api.spawnEntity(null, "cow", {
+	"x": 4,
+	"y": 4,
+	"rot": FacingDirection.South
+});
+
+// You can also spawn mobs at random positions, like this
+api.spawnEntity(null, "creeper", "random");
+
+`;
+
+const defaults = {
+	assetPacks: {
+		beforeLoad: ["allAssetsMinusPlayer", "playerAlex", "playerAgent"],
+		afterLoad: [],
+	},
+	gridDimensions: [10, 10],
+	fluffPlane: new Array(100).fill(""),
+	playerName: "Alex",
+	playerStartPosition: [],
+	script: defaultScript
 };
 
+var levelParam = getParameterByName("level");
+var testLevelToLoad = Object.assign({}, defaults, levels[levelParam] || levels["default"]);
+
 function getParameterByName(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+	var tmp = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+	var regex = new RegExp(`[\\?&]${tmp}=([^&#]*)`);
+	var results = regex.exec(location.search);
+	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
-var levelParam = getParameterByName('level');
-var testLevelToLoad = levels[levelParam] || levels['default'];
-testLevelToLoad = Object.assign({}, defaults, testLevelToLoad);
-
-// Initialize test instance of game, exposed to window for debugging.
-var gameController = new GameController({
-  Phaser: window.Phaser,
-  containerId: 'phaser-game',
-  assetRoot: 'assets/',
-  audioPlayer: new Sounds(),
-  debug: true,
-  earlyLoadAssetPacks: testLevelToLoad.earlyLoadAssetPacks,
-  earlyLoadNiceToHaveAssetPacks: testLevelToLoad.earlyLoadNiceToHaveAssetPacks,
-  afterAssetsLoaded: () => {
-    gameController.codeOrgAPI.startAttempt();
-  },
-});
-
-gameController.loadLevel(testLevelToLoad);
-
-var $levelselect = $('#level-load');
-Object.keys(levels).forEach(key => {
-  $levelselect.append($('<option/>', {text: key, selected: key === levelParam}));
-});
-
-$levelselect.on('change', () => {
-  location.search = `level=${$levelselect.val()}`;
-});
-
-$('input[type=range]').on('input', function () {
-  $("#speed-display").html('Speed: ' + $(this).val() + 'x');
-  gameController.game.time.slowMotion = 1.5 / parseFloat($(this).val(), 10);
-});
-
-$('#reset-button').click(function () {
-  gameController.codeOrgAPI.resetAttempt();
-  gameController.codeOrgAPI.startAttempt();
-});
-
-if (!gameController.levelData.isAgentLevel) {
-  $('#entity-select').hide();
+function errCallback(err) {
+	consoleErrors.innerText += `[ERR] ${err}` + "\n";
+	consolePre.scrollTop = consolePre.scrollHeight - consolePre.clientHeight;
+	return;
 }
 
-window.addEventListener('keydown', e => {
-  if (e.target !== document.body) {
-    e.preventDefault();
-  }
-  e.stopImmediatePropagation();
+function initGame() {
+	// Initialize test instance of game, exposed to window for debugging.
+	gameController = new GameController({
+		Phaser: window.Phaser,
+		containerId: "phaser-game",
+		assetRoot: "assets/",
+		audioPlayer: new Sounds(),
+		debug: true,
+		earlyLoadAssetPacks: testLevelToLoad.earlyLoadAssetPacks,
+		earlyLoadNiceToHaveAssetPacks: testLevelToLoad.earlyLoadNiceToHaveAssetPacks,
+		afterAssetsLoaded: function () {},
+	});
 
-  var target = $('input[name=target]:checked').val();
-  var instance = target === 'Player' ? gameController.player : gameController.agent;
+	api = Object.assign(gameController.codeOrgAPI, {
+		log: function (msg) {
+			consoleErrors.innerText += "[LOG] " + msg + "\n";
+		},
+		error: function (msg) {
+			consoleErrors.innerText += "[ERR] " + msg + "\n";
+		},
+		warning: function (msg) {
+			consoleErrors.innerText += "[WARN] " + msg + "\n";
+		}
+	});
 
-  if (instance.queue.getLength() > 0) {
-    return;
-  }
+	gameController.loadLevel(testLevelToLoad);
 
-  switch (e.keyCode) {
-    case 8:
-    case 46:
-      gameController.codeOrgAPI.destroyBlock(null, target);
-      break;
-    case 13:
-      gameController.codeOrgAPI.placeInFront(null, $('#block-type').val(), target);
-      break;
-    case 16:
-      $('input[name=target]:not(:checked)').prop('checked', true);
-      break;
-    case 38:
-    case 87:
-      gameController.codeOrgAPI.moveDirection(null, target, 0);
-      break;
-    case 40:
-    case 83:
-      gameController.codeOrgAPI.moveDirection(null, target, 2);
-      break;
-    case 37:
-    case 65:
-      gameController.codeOrgAPI.moveDirection(null, target, 3);
-      break;
-    case 39:
-    case 68:
-      gameController.codeOrgAPI.moveDirection(null, target, 1);
-      break;
-  }
-}, true);
+	Object.keys(levels).forEach(key => {
+		var option = document.createElement("option");
+		option.text = key;
+		option.selected = key === levelParam;
+		levelSelect.appendChild(option);
+	});
+	return;
+}
 
-window.gameController = gameController;
+function setCallbacks() {
+	playButton.addEventListener("click", function () {
+		if (previous) {
+			api.resetAttempt();
+		}
+		previous = true;
+		consoleErrors.innerText = "";
+		playButton.blur();
+		phaserGame.focus();
+		gameController.levelData.script = codeMirror.getValue();
+		gameController.game.canvas.id = "game-canvas";
+		gameController.player.selectedItem = blockType.options[blockType.selectedIndex].text;
+		api.startAttempt(null, errCallback, api);
+	});
+
+	blockType.addEventListener("change", function () {
+		gameController.player.selectedItem = blockType.options[blockType.selectedIndex].text;
+	});
+
+	document.getElementById("stop-button").addEventListener("click", function () {
+		api.resetAttempt();
+		previous = false;
+	});
+	return;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	var codeMirrorWidth;
+
+	levelSelect = document.getElementById("level-load");
+	speedSlider = document.getElementById("speed-slider");
+	speedDisplay = document.getElementById("speed-display");
+	phaserGame = document.getElementById("phaser-game");
+	codeMirrorEditor = document.getElementById("codemirror-editor");
+	game = document.getElementById("game");
+	playButton = document.getElementById("play-button");
+	consolePre = document.getElementById("console-pre");
+	consoleErrors = document.getElementById("console-errors");
+	blockType = document.getElementById("block-type");
+
+	codeMirror = CodeMirror.fromTextArea(codeMirrorEditor, {
+		lineNumbers: true,
+		mode: "javascript"
+	});
+	codeMirrorWidth = document.body.clientWidth - Number.parseInt(getComputedStyle(document.body).fontSize) * 2 - game.offsetWidth;
+	codeMirror.setSize(codeMirrorWidth, game.offsetHeight + 200);
+	consoleErrors.style.width = codeMirrorWidth;
+	consolePre.style.width = codeMirrorWidth;
+	consoleErrors.style.height = 200;
+	consolePre.style.height = 200;
+
+	codeMirror.getDoc().setValue(testLevelToLoad.script);
+
+	setCallbacks();
+	initGame();
+});
+
+document.addEventListener("input", function (event) {
+	if (event.target.id == "level-load") {
+		location.search = `level=${levelSelect.options[levelSelect.selectedIndex].value}`;
+	} else if (event.target.id == "speed-slider") {
+		speedDisplay.innerHTML = `Speed: ${speedSlider.value}x`;
+		gameController.game.time.slowMotion = 1.5 / Number.parseFloat(speedSlider.value, 10);
+	}
+}, false);
--- js/game/CommandQueue/CommandState.js
+++
@@ -1,6 +0,0 @@
-CommandState = Object.freeze({
-  NOT_STARTED: 0,
-  WORKING: 1,
-  SUCCESS: 2,
-  FAILURE: 3
-});
